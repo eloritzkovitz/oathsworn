fb3a6315eb51b111c8b483c710507b4d0ba79433
eloritzkovitz <elor2200@gmail.com>
2025-08-14 20:42:23 +0300

    Update

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using StarterAssets;
 
 public class PlayerAnimatorController : MonoBehaviour
@@ -6,10 +7,31 @@ public class PlayerAnimatorController : MonoBehaviour
     private Animator animator;
     private ThirdPersonController controller;
 
-    private void Start()
+    private void Awake()
     {
         animator = GetComponent<Animator>();
         controller = GetComponentInParent<ThirdPersonController>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // Set animation path based on scene
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Set animation path according to scene
+        if (scene.name == "Scene2")
+        {
+            animator.SetBool("isWanted", true);
+            Debug.Log("You are now wanted!");
+        }
+        else
+        {
+            animator.SetBool("isWanted", false);
+        }
     }
 
     private void Update()
@@ -22,19 +44,17 @@ public class PlayerAnimatorController : MonoBehaviour
         bool isRunning = Input.GetKey(KeyCode.LeftShift) && isWalking;
 
         // Crouching
-        bool isCrouching = Input.GetKey(KeyCode.C);
+        bool isCrouching = Input.GetKey(KeyCode.C);        
 
-        // Grounded status from controller
+        // Jumping
         bool isGrounded = controller.Grounded;
-
-        // Check if jumping: not grounded and not falling
-        bool isJumping = !isGrounded;
+        bool isJumping = Input.GetKey(KeyCode.Space) && controller.Grounded;
+        animator.SetBool("Jump", isJumping);     
 
         // Set animator parameters
         animator.SetBool("isWalking", isWalking && !isRunning && !isCrouching && isGrounded);
         animator.SetBool("isRunning", isRunning && !isCrouching && isGrounded);
         animator.SetBool("isCrouching", isCrouching && isGrounded);
-        animator.SetBool("isJumping", isJumping);
 
         // Attack trigger (on left click down)
         if (Input.GetMouseButtonDown(0))
